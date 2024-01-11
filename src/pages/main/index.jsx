f6557f19d2330b51bc0a053dd15118e82ce42151
ERuvinga <ruvingaelie@gmail.com>
2024-01-11 13:08:44 +0200

    add Heroicon and animate 404Img

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,8 +1,10 @@
 // main page
+import {BeakerIcon} from "@heroicons/react/24/outline"
 
 const MainPage = () =>{
     return (
         <div>
+            <BeakerIcon/>
             Main Page
         </div>
     )
