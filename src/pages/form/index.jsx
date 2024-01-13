8b390d6f6170ffc8913c3294d9c1771c01440b2d
ERuvinga <ruvingaelie@gmail.com>
2024-01-13 15:27:47 +0200

    styling first part of formPage

--- src/pages/form/index.jsx
+++ src/pages/form/index.jsx
@@ -1,7 +1,25 @@
 // page for formular
 import React from 'react';
+import '../../Style/styleFormPage.css';
 const FormPage = () => {
-    return <div>formulaire Page</div>;
+    return (
+        <main className="containerForm">
+            <section className="presentation">
+                <div className="containerPresentation">
+                    <h1>GoFinance</h1>
+                    <span>The most popular peer to peer lending at SEA</span>
+                    <a href="#" className="linkBtn">
+                        Read More
+                    </a>
+                </div>
+                <div className="border illustration">
+                    <div className="circles cr1"></div>
+                    <div className="circles cr2"></div>
+                </div>
+            </section>
+            <section className="border formDatas"></section>
+        </main>
+    );
 };
 
 export default FormPage;
